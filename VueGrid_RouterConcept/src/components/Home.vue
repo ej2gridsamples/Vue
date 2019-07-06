9c338abac1dff728c17070007fdd9feb34beee75
ej2gridsamples <37431931+ej2gridsamples@users.noreply.github.com>
2019-07-06 15:32:26 +0530

    Grid with Vue router concept
    How to use routers in Vue?

---
+++ VueGrid_RouterConcept/src/components/Home.vue
@@ -0,0 +1,39 @@
+// Home.vue
+
+<template>
+    <ejs-grid ref='grid' id='Grid' :dataSource="data" :allowPaging="true">
+        <e-columns>
+            <e-column field='OrderID' headerText='Order ID' width=90></e-column>
+            <e-column field='CustomerID' headerText='Customer ID' width=120></e-column>
+            <e-column field='Freight' headerText='Freight' format='C2' width=90></e-column>
+        </e-columns>
+    </ejs-grid>
+</template>
+
+<script>
+import Vue from 'vue';
+import { GridPlugin, Page } from '@syncfusion/ej2-vue-grids';
+import { data } from '../datasource.js';
+
+Vue.use(GridPlugin);
+
+export default {
+    data () {
+        return {
+            data: data
+        }
+    },
+     beforeRouteLeave (to, from, next) {
+         this.$refs.grid.ej2Instances.destroy = function(args){
+             alert("Destroy method invoked")
+         }
+         next();
+     },
+    methods: {
+        
+    },
+    provide: {
+        grid: [Page]
+    }
+}
+</script>
